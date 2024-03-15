e3220eb71ee3cfe39cb25ea0b81ba1bac6d14a7e
Anand Kane <akane@merchante.com>
2024-03-15 20:36:08 +0530

    Added vault commands

---
+++ vault/set-vault-env.sh
@@ -0,0 +1,6 @@
+export VAULT_ADDR='https://127.0.0.1:8200'
+export VAULT_TOKEN=$1
+export VAULT_CACERT=/home/akane/tutorials/vault/certs/vault-ca.pem
+export AWS_ACCESS_KEY_ID=AKIATGUY7AHFKUTYXCAK
+export AWS_SECRET_ACCESS_KEY=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
+
